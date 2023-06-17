cc53cc716bec0fd2c17771b881838a3ca4126986
Amanda <107001164+amandadeusilva@users.noreply.github.com>
2023-06-17 18:25:08 -0300

    acrescentando pastas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import emailjs from '@emailjs/browser'
 
 function App() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  function sendEmail(e){
+    e.preventDefault();
+    alert("teste")
+
+    if(name === '' || email === '' || message === ''){
+        alert("Preencha todos os campos");
+        return;
+    }
+
+    const templateParams = {
+        from_name: name,
+        message: message,
+        email: email,
+    }
+
+    emailjs.send("service_6he2hun","template_uxqj4ur", templateParams,"vt1x60x26fV0kZPrw")
+    .then((response)=>{
+        console.log("EMAIL ENVIADO", response.status, response.text)
+        setName('')
+        setEmail('')
+        setMessage('')
+    }, (err) =>{
+        console.log('ERRO:', err)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Contato</h1>
+
+      <form className="form" onSubmit={sendEmail}>
+        <input 
+          className="input"
+          type="text"
+          placeholder="Digite seu nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        
+        <input 
+          className="input"
+          type="text"
+          placeholder="Digite seu email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+<textarea 
+          className="textarea"
+          placeholder="Digite sua mensagem..."
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+
+        <input className="button" type="submit" value="Enviar" />
+      </form>
+
     </div>
   );
 }
 
 export default App;
+
+
+
